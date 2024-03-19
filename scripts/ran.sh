c794d55ca8550313bb7b3904b66e66196af181e6
MShayanNazeer <muhammadshayannazeer@gmail.com>
2024-03-19 13:01:45 -0400

    added support for multiple UEs

--- scripts/ran.sh
+++ scripts/ran.sh
@@ -5,7 +5,6 @@
 # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 set -e
 
-
 # load config values
 source /local/repository/scripts/setup-config
 
@@ -17,15 +16,45 @@ echo "1. Install the UERANSIM dependencies."
 cd ~
 sudo apt -y --force-yes update 
 DEBIAN_FRONTEND=noninteractive sudo apt -y --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-sudo apt -y --force-yes install make g++ openjdk-11-jdk maven libsctp-dev lksctp-tools snapd iperf3
-sudo apt install make gcc g++ libsctp-dev lksctp-tools iproute2 git
-
+sudo apt -y --force-yes install make g++ openjdk-11-jdk maven libsctp-dev lksctp-tools snapd
+# getting cmake from apt installs an old version of cmake, so we have to get it from snap
 sudo snap install cmake --classic
+# maybe gets rid of grub popup manual enter req
+# https://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
 
-git clone https://github.com/aligungr/UERANSIM
+#echo "2. Set the JAVA_HOME environment variable."
+#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 
+echo "3. Clone UERANSIM."
+cd ~
+git clone --depth 1 --branch v3.2.0 https://github.com/aligungr/UERANSIM.git
+
+echo "4. Change configs"
+cp /local/repository/config/ueran-profile.yaml ~/UERANSIM/config/profile.yaml
 cp /local/repository/config/ueran-gnb.yaml ~/UERANSIM/config/open5gs-gnb.yaml
-cp /local/repository/config/ueran-ue.yaml ~/UERANSIM/config/open5gs-ue.yaml
+mkdir ~/UERANSIM/config/open5gs-ue
+cp /local/repository/config/ueran-ue.yaml ~/UERANSIM/config/open5gs-ue/ue-default.yaml
 
+replace_in_file() {
+    # $1 is string to find, $2 is string to replace, $3 is filename
+    sed -i "s/$1/$2/g" $3
+}
+
+cd ~/UERANSIM/config/open5gs-ue
+# autogenerate config files for each ue
+upper=$(($NUM_UE_ - 1))
+for i in $(seq 0 $upper); do
+    file=ue"$i.yaml"
+    defaultkey="465B5CE8B199B49FAA5F0A2EE238A6BC"
+    newkey=$(printf "%0.s$i" {1..32})
+    cp ue-default.yaml $file
+    replace_in_file $defaultkey $newkey $file
+    defaultimsi="imsi-999700000000001"
+    newimsi="imsi-99970000000000$i"
+    replace_in_file $defaultimsi $newimsi $file
+done
+
+
+echo "4.Build UERANSIM"
 cd ~/UERANSIM
 make
